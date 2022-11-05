45879a839bbbe0244cc8f3e710570e2010246489
katayama <tattu.0310@gmail.com>
2022-11-05 22:58:35 +0900

    update

--- practice/js/main.js
+++ practice/js/main.js
@@ -11,6 +11,10 @@ window.addEventListener("DOMContentLoaded", function () {
     let selectedUser = document.getElementById("selectedUser")
     let firstNames = document.getElementsByClassName("firstName");
     let lastNames = document.getElementsByClassName("lastName");
+    let firstEdit = document.getElementById("firstEdit");
+    let secondEdit = document.getElementById("secondEdit");
+    let thirdEdit = document.getElementById("thirdEdit");
+
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -21,25 +25,52 @@ window.addEventListener("DOMContentLoaded", function () {
         document.getElementById("firstName").value = "";
         const switchId = Number(selectedUser.innerHTML[0])
         console.log(firstNames);
+        console.log(typeof switchId);
+        //　数字以外の場合は処理を終了
+        if (isNaN(switchId)) {
+            alert("please select user");
+            return;
+        }
         lastNames[switchId - 1].innerHTML = lastName;
         firstNames[switchId - 1].innerHTML = firstName;
     });
 
-    firstId.addEventListener("click", (e) => {
+    function handleInputName(e, id) {
         e.preventDefault();
-        console.log(selectedUser);
-        selectedUser.innerHTML = `${firstId.innerHTML}番目のユーザーが選択されました`;
+        selectedUser.innerHTML = `${id.innerHTML}番目のユーザーが選択されました`;
+    }
+
+    function handleEditName(e, id) {
+        e.preventDefault();
+        let firstName = firstNames[id].innerHTML;
+        let lastName = lastNames[id].innerHTML;
+        document.getElementById("lastName").value = lastName;
+        document.getElementById("firstName").value = firstName;
+    }
+
+    firstId.addEventListener("click", (e) => {
+        handleInputName(e, firstId);
     });
 
     secondId.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log(selectedUser);
-        selectedUser.innerHTML = `${secondId.innerHTML}番目のユーザーが選択されました`;
+        handleInputName(e, secondId);
     });
 
     thirdId.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log(selectedUser);
-        selectedUser.innerHTML = `${thirdId.innerHTML}番目のユーザーが選択されました`;
+        handleInputName(e, thirdId);
     });
+
+    firstEdit.addEventListener("click", (e) => {
+        handleEditName(e, 0);
+    });
+
+    secondEdit.addEventListener("click", (e) => {
+        handleEditName(e, 1);
+    });
+
+    thirdEdit.addEventListener("click", (e) => {
+        handleEditName(e, 2);
+    });
+
+
 });
