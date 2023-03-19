d31b2ed037137e694c1c06c4e417214aad441ffa
katayama8000 <tattu.0310@gmail.com>
2023-03-19 17:52:08 +0900

    www

---
+++ Finance/AlphaVantage/index1.js
@@ -0,0 +1,20 @@
+'use strict';
+let request = require('request');
+
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=WVZ7UG5YCO8D2L1L';
+
+request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+    if (err) {
+        console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+    } else {
+        // data is successfully parsed as a JSON object:
+        console.log(data);
+    }
+});
