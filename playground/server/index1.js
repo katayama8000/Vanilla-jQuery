dad624ac5769641badd32ab495e2b3d2ab49f316
katayama8000 <tattu.0310@gmail.com>
2023-04-05 21:12:20 +0900

    feat

--- playground/server/index1.js
+++ playground/server/index1.js
@@ -12,3 +12,10 @@ const result = arr.reduce((accumulator, currentValue) => {
 }, {});
 
 console.log(result); // { Alice: 20, Bob: 30, Charlie: 40 }
+
+const numbers2 = [40, 20, 50, 10, 30, 11];
+numbers2.sort((a, b) => {
+    console.log(a, b)
+    return a - b
+});
+console.log(numbers2);
