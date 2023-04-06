b255411c1483677ffbbfdc838b4bd5fc06df39c3
katayama8000 <tattu.0310@gmail.com>
2023-04-06 12:58:55 +0900

    twitter hack

---
+++ playwright/Twitter/index1.mjs
@@ -0,0 +1,29 @@
+import { chromium } from 'playwright-core';
+
+(async () => {
+    console.time('playwright')
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    try {
+        await page.goto('https://twitter.com/home?lang=ja');
+        // ページ遷移を待つ
+        // await frame.waitForURL('https://twitter.com/home?lang=ja');
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 1000);
+        });
+        //　スクショ
+        await page.screenshot({ path: `./playwright/Twitter/img/screenshot.png` });
+
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        await browser.close();
+        console.timeEnd('playwright')
+    }
+})();
