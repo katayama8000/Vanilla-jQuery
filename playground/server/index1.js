1f2f42e0c7c763a72290c805d2c6a17b03a8be3a
katayama8000 <tattu.0310@gmail.com>
2023-04-17 19:09:28 +0900

    dayjs

--- playground/server/index1.js
+++ playground/server/index1.js
@@ -1,4 +1,5 @@
 const makeShortString = require("make-short-string");
+const dayjs = require('dayjs')
 // 【配列の要素からオブジェクトを作成する例】
 
 const arr = [
@@ -26,3 +27,19 @@ console.log(names.at(-1) === names[names.length - 1]); // true
 
 const str = makeShortString("hello-world", 10);
 console.log(str); // hello-worl...
+
+// console.log(dayjs().format('YYYY-MM-DD HH:mm:ss')); // 2021-03-01 17:00:00
+
+const date = new Date();
+console.log(date.toLocaleString()); // 2021/3/1 17:00:00    
+
+const yesterday = date.setDate(date.getDate() - 1);
+console.log(new Date(yesterday).toLocaleString()); // 2021/2/28 17:00:00
+
+// 比較
+console.log(dayjs().isBefore(dayjs().subtract(1, 'day'))); // true
+console.log(dayjs(yesterday).isBefore(dayjs())); // true
+
+console.log(dayjs()); // 2021-03-01 17:00:00
+
+console.log(dayjs().isAfter(dayjs())); // false
