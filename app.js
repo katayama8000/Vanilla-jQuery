f2c38ef7948669b3bacece5f6ffda379c105050b
katayama8000 <tattu.0310@gmail.com>
2023-04-18 23:00:40 +0900

    qqq

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // dayjsの検証
 const dayjs = require('dayjs');
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore')
+const isSameOrAfter = require('dayjs/plugin/isSameOrAfter')
+dayjs.extend(isSameOrBefore)
+dayjs.extend(isSameOrAfter)
 // console.log(date);
 // // 日本時間
 // console.log(date.toLocaleString('ja-JP'));
@@ -26,13 +30,24 @@ const dayjs = require('dayjs');
 // console.log(dayjs(yesterday).isAfter(day3));
 // console.log(dayjs(yesterday).isAfter(day3.toDate()));
 
-const timezone = require('dayjs/plugin/timezone');
-const utc = require('dayjs/plugin/utc');
-dayjs.extend(utc);
-dayjs.extend(timezone);
+// const timezone = require('dayjs/plugin/timezone');
+// const utc = require('dayjs/plugin/utc');
+// dayjs.extend(utc);
+// dayjs.extend(timezone);
 
-// 日本時間を設定する
-const tokyoTime = dayjs().tz("Asia/Tokyo");
-const now = dayjs().tz("Asia/Tokyo")
-console.log(now.endOf('day').toDate());
-console.log(dayjs().endOf('day'));
+// // 日本時間を設定する
+// const tokyoTime = dayjs().tz("Asia/Tokyo");
+// const now = dayjs().tz("Asia/Tokyo")
+// console.log(now.endOf('day').toDate());
+// console.log(dayjs().endOf('day'));
+
+// 一時間後
+console.log(dayjs().add(1, 'hour').toDate());
+const addAHour = dayjs().add(1, 'hour').toDate();
+const addADay = dayjs().add(1, 'day').toDate();
+// console.log(dayjs(addAHour).isSame(dayjs(), "date"));
+// console.log(dayjs(addAHour).isSame(dayjs()));
+// console.log(dayjs(addADay).isSame(dayjs(), "date"));
+// console.log(dayjs(addADay).isAfter(dayjs(), "date"));
+console.log(dayjs(addAHour).isAfter(dayjs(), "day"));
+console.log(dayjs(addADay).isSameOrAfter(dayjs(), "day"));
