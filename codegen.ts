a6f15f3a1930df8a25acb504dd22eae5910ca1af
katayama800 <tattu.0310@gmail.com>
2023-12-03 15:00:38 +0900

    wip: codegen

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,15 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:3000/graphQL",
-  documents: "src/**/*.tsx",
+  schema: 'http://localhost:3000/graphQL',
+  documents: 'src/**/*.ts',
   generates: {
-    "src/gql/": {
-      preset: "client",
-      plugins: []
-    }
-  }
+    'src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
 };
 
 export default config;
